f43823d4bbefa744be0d42527edfd1360f8390ea
HuduOsman5 <huduosman5523@gmail.com>
2023-07-19 08:30:56 -0700

    text

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "stdio.h" // For the putchar declaration
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
+ */
+void print_alphabet_x10(void)
+{
+    char letter;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        letter = 'a';
+        while (letter <= 'z')
+        {
+            putchar(letter); // Use putchar directly
+            letter++;
+        }
+        putchar('\n'); // Use putchar directly
+    }
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_alphabet(void);
+/* Function prototype for print_alphabet_x10 */
+void print_alphabet_x10(void);
+
+/* Other function prototypes, if any */
 
 #endif /* MAIN_H */
