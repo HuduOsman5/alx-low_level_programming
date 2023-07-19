c30c931dc3410c7966bfbf9f2c42c34558268dd6
HuduOsman5 <huduosman5523@gmail.com>
2023-07-19 13:50:01 -0700

    text

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype */
+#include <stdio.h>
+
+/* Function prototypes */
 int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+void print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 
 #endif /* MAIN_H */
