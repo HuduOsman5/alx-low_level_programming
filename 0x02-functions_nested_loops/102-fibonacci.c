873290a879be1499076328d96762a266b9c7548a
HuduOsman5 <huduosman5523@gmail.com>
2023-07-19 15:36:52 -0700

    text

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,14 +1,34 @@
-#include "main.h"
+/*
+ * File: 102-fibonacci.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
 
 /**
- * print_alphabet - prints the lowercase alphabet followed by a new line
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
-	char letter;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
 
-	_putchar('\n');
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
 }
