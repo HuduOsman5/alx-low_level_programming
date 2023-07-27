c502b1c1a6172389013550e09867c216359e13fc
HuduOsman5 <huduosman5523@gmail.com>
2023-07-27 04:10:22 -0700

    text

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,12 @@
 #include "main.h" 
+
 /**
  * _strcat - concatenates 2 strings.
  * @dest: string with concatenation
  * @src: string to be concatenated
  * Return: Always 0.
- */
+*/
+
 char *_strcat(char *dest, char *src)
 {
 	int lengthD, lengthS;
