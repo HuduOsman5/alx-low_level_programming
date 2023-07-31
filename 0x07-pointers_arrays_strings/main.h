7233be685d83519a0f9fe584d553f281e9ad9e32
HuduOsman5 <huduosman5523@gmail.com>
2023-07-31 11:56:11 -0700

    text

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,19 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototypes */
+
 char *_memset(char *s, char b, unsigned int n);
-/* Add other function prototypes as needed */
-
-/* Type definitions (if any) */
-typedef struct {
-    int x;
-    int y;
-} Point;
-/* Add other type definitions as needed */
-
-/* Constant declarations (if any) */
-#define MAX_BUFFER_SIZE 1024
-/* Add other constant declarations as needed */
-
-#endif /* MAIN_H */
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+#endif
