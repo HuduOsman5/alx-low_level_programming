34d88ea7230dc7036b2cad0232569fe5f2072baf
HuduOsman5 <huduosman5523@gmail.com>
2023-07-31 11:29:09 -0700

    text

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+char *_memset(char *s, char b, unsigned int n);
+/* Add other function prototypes as needed */
+
+/* Type definitions (if any) */
+typedef struct {
+    int x;
+    int y;
+} Point;
+/* Add other type definitions as needed */
+
+/* Constant declarations (if any) */
+#define MAX_BUFFER_SIZE 1024
+/* Add other constant declarations as needed */
+
+#endif /* MAIN_H */
